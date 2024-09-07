5423153cd8c3fe6e3c64577e4646d5b7421ce4a1
Talha Okur <t@talhaokur.net>
2024-09-07 22:57:09 +0300

    bug fix: wrong job repository path issue fixed.
    description: By the time of creation of JobContainer service global variables were empty hence job repository paths were wrong. Now loading values after they're initialized.

--- services/jobcontainer.service.js
+++ services/jobcontainer.service.js
@@ -4,11 +4,6 @@ import path from 'path';
 import { GLOBALS } from '../configs.js';
 
 class JobContainerService {
-    constructor() {
-        this.mainRepository = path.join(GLOBALS.workingDir, 'output');
-        console.log(`main repo is ${this.mainRepository}`);
-    }
-
     async initializeJob() {
         const jobId = this._generateJobId();
         const jobRepoPath = await this._createJobRepository(jobId);
@@ -20,18 +15,22 @@ class JobContainerService {
         return uuidv4();
     }
 
+    _getOutputRepositoryPath() {
+        return path.join(GLOBALS.workingDir, 'output');
+    }
+
     async _createJobRepository(jobId) {
-        const jobRepoPath = path.join(this.mainRepository, jobId);
+        const jobRepoPath = path.join(this._getOutputRepositoryPath(), jobId);
         if (fs.existsSync(jobRepoPath))
             throw new Error(`Job ${jobId} has already a repository.`); // TODO change this to a proper one
 
         fs.mkdirSync(jobRepoPath);
-        console.info(`Job repository is created for jobId:${jobId}`);
+        console.info(`Job repository is created for jobId:${jobId} path:${jobRepoPath}`);
         return jobRepoPath;
     }
 
     async _createImageRepository(jobId) {
-        const imageRepoPath = path.join(this.mainRepository, jobId, 'images');
+        const imageRepoPath = path.join(this._getOutputRepositoryPath(), jobId, 'images');
         if (fs.existsSync(imageRepoPath))
             throw new Error(`Image repository is already there for jobId:${jobId}`);  // TODO change this to a proper one
         fs.mkdirSync(imageRepoPath);
@@ -43,10 +42,19 @@ class JobContainerService {
         if (!jobId)
             throw new Error('jobId cannot be null or empty!'); // TODO change this to a proper one
 
-        const jobRepoPath = path.join(this.mainRepository, jobId);
+        const jobRepoPath = path.join(this._getOutputRepositoryPath(), jobId);
 
         fs.rmSync(jobRepoPath, { recursive: true, force: true });
         console.info(`Job repository deleted successfuly with id: ${jobId}`);
+    }
+
+    deleteImageRepositoryForJob(jobId) {
+        if (!jobId)
+            throw new Error('path cannot be null or empty!'); // TODO change this to a proper one
+
+        const imageRepoPath = path.join(this._getOutputRepositoryPath(), jobId, 'images');
+        fs.rmSync(imageRepoPath, {recursive: true, force: true});
+        console.info('Image repository is deleted for job:', jobId);
     }
 }
 
