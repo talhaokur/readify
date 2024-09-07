e29e14b1e9cf8bdd0341be4bbaa66e23326bbb3a
Talha Okur <t@talhaokur.net>
2024-09-07 21:38:24 +0300

    get epub completed

--- routes/epub.route.js
+++ routes/epub.route.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 import { HttpStatusCode } from 'axios';
+import fs from 'node:fs';
+import path from 'path';
+import { GLOBALS } from '../configs.js';
+import { epubService } from '../services/epub.service.js';
 
 const router = Router();
 
@@ -9,20 +13,31 @@ function isValidUUID(uuid) {
 }
 
 router.get('/', (req, res) => {
+    // TODO
     return res.status(HttpStatusCode.NotImplemented).json();
 });
 
-router.get("/:uuid", (req, res) => {
+router.get("/:uuid", async (req, res, next) => {
     const { uuid } = req.params;
     if (!isValidUUID(uuid))
-        return res.status(HttpStatusCode.BadRequest).json({message: `Given id is not valid`});
-    return res.status(HttpStatusCode.NotImplemented).json();
+        return res.status(HttpStatusCode.BadRequest).json({ message: `Given id is not valid` });
+    const jobRepository = path.join(GLOBALS.outputDir, uuid);
+
+    if (!fs.existsSync(jobRepository))
+        return res.status(HttpStatusCode.NotFound).json();
+
+    const epubPath = epubService.getEpubFilePath(jobRepository, (p) => { return p; });
+
+    if (!epubPath)
+        return res.status(HttpStatusCode.NotFound).json({ message: "No ePub found!" });
+    
+    return res.download(epubPath);
 });
 
 router.delete("/:uuid", (req, res) => {
     const { uuid } = req.params;
     if (!isValidUUID(uuid))
-        return res.status(HttpStatusCode.BadRequest).json({message: `Given id is not valid`});
+        return res.status(HttpStatusCode.BadRequest).json({ message: `Given id is not valid` });
     return res.status(HttpStatusCode.NotImplemented).json();
 });
 
--- services/epub.service.js
+++ services/epub.service.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'path';
 const { default: Epub } = await import('epub-gen');
 
 class EpubService {
@@ -14,6 +16,25 @@ class EpubService {
                 console.error(err);
                 throw err;
             });
+    }
+
+    getEpubFilePath(jobRepo) {
+        if (!jobRepo)
+            throw new Error(); // TODO change this to a proper error
+
+        const files = fs.readdirSync(jobRepo);
+        const epubFiles = files.filter(file => {
+            return path.extname(file).toLowerCase() === '.epub';
+        });
+
+        if (epubFiles.length === 0) {
+            console.log("No epub file found!");
+            return null;
+        }
+        else if (epubFiles.length > 1)
+            throw new Error(); // TODO change this to a proper error
+
+        return path.join(jobRepo, epubFiles[0]);
     }
 }
 
